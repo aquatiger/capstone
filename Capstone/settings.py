c9d438417dab5d5fbacad3e88b54d314e10c961b
aquatiger <rls4jc@gmail.com>
2018-02-20 10:25:06 -0800

    Tried something
    Tried to make more secure

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lk-1g$+p9-te2as9)g)b6%q5_#f-!6-ite0#!9m3@h3$-225l='
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
